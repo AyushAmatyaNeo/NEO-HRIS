0061f150e36e6ec247689ceaefe493c6ace3cbc4
Sabita Magar <91114443+SabitaMagr@users.noreply.github.com>
2022-09-05 10:20:18 +0545

    asdf

---
+++ config/autoload/global.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'db' => [
+        'driver' => 'oci8',
+        'connection_string' => '(DESCRIPTION =
+        (ADDRESS = (PROTOCOL = TCP)(HOST = 10.255.0.103)(PORT = 1521))
+        (CONNECT_DATA =
+        (SERVER = DEDICATED)
+        (SERVICE_NAME = orcl)
+        )
+        )',
+        'username' => 'JGI_GIT',
+        'password' => 'JGI_GIT',
+        'platform_options' => ['quote_identifiers' => false]
+    ],
+    'service_manager' => [
+        'factories' => [
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ],
+    ],
+];
---
+++ public/js/setup/travel-category/add.js
@@ -0,0 +1,47 @@
+(function ($, app) {
+    'use strict';
+    $(document).ready(function () {
+        $('select').select2();
+        var $categoryName=$('#categoryName');
+        var $dailyAllowance=$('#dailyAllowance');
+        var $advanceAmount=$('#advanceAmount');
+        var $submit=$('#submit');
+        var $travelCategoryForm=$('#travelCategoryForm');
+
+      
+        $submit.on('click',function(){
+            if($("#travelCategoryForm").valid()){
+                travelCategory(this);
+            }
+        })
+        var travelCategory=function(obj){
+            var $this=$(obj);
+            app.pullDataById(document.addTravelCategoryLink,{
+                'categoryName':$categoryName.val(),
+                'dailyAllowance':$dailyAllowance.val(),
+                'advanceAmount':$advanceAmount.val(),}).then(function(response){
+                app.showMessage("Travel Category Assigned Successfully.");
+                window.location.href = '../travelCategory';
+            },function(error){
+    
+            });
+        }
+        var validate =  $travelCategoryForm.validate({
+            rules: {
+                categoryName: {
+                    required: true
+                },
+                dailyAllowance:{
+                    required: true
+                },
+                advanceAmount:{
+                    required:true
+                }
+            },
+            messages: {
+              
+            }
+        });
+    });
+})(window.jQuery, window.app);
+
